9b20a07805abb94383cf3131da89ac92ebe632ef
Ilie Bostan <iliebostan@gmail.com>
2019-03-16 18:23:59 +0200

    test1

---
+++ Exerci1/JavaScriptBasics1.js
@@ -0,0 +1,12 @@
+console.log ("take me home");
+
+
+var d = new Date();
+document.getElementById("beta").innerHTML = d;
+
+function HandleInputChanged ()
+{
+    alert ("Welcome " + document.getElementById("nameInput"). value);
+
+}
+HandleInputChanged ();
